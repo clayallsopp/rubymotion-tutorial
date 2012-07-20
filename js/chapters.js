ef897d4dee367fbc33c4e9c727dfaad47239ef31
Clay Allsopp <clay.allsopp@gmail.com>
2012-07-19 21:14:10 -0700

    when mobile, hide chapter nav at first.

--- js/chapters.js
+++ js/chapters.js
@@ -1,11 +1,15 @@
 $(function () {
-    $("#chapter-nav-btn").click(function() {
-        $("#chapter-nav").toggle();
+    var fixChevrons = function() {
         if ($("#chapter-nav").is(":visible")) {
             $("#chapter-nav-chevron").removeClass("icon-chevron-down").addClass("icon-chevron-up");
         }
         else {
             $("#chapter-nav-chevron").removeClass("icon-chevron-up").addClass("icon-chevron-down");
         }
+    }
+    fixChevrons();
+    $("#chapter-nav-btn").click(function() {
+        $("#chapter-nav").toggle();
+        fixChevrons();
     })
 });
