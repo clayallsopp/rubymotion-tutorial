8540968708c8d993554c23b8b61d3281e7e33f09
Marc Rendl Ignacio <marcrendlignacio@gmail.com>
2012-09-04 16:32:50 +0800

    Update 10-api-driven-example/Colr/app/models/color.rb
    extraneous to_s method.
    
    Made sure this is the case with RubyMotion's ruby:
    
    ```
    $ rake
    main> {key: "value"}.each {|key, value| puts (key.to_s+"=").class; puts value.class}
    String
    String
    => {:key=>"value"}
    ```

--- 10-api-driven-example/Colr/app/models/color.rb
+++ 10-api-driven-example/Colr/app/models/color.rb
@@ -7,7 +7,7 @@ class Color
   def initialize(hash = {})
     hash.each { |key, value|
       if PROPERTIES.member? key.to_sym
-        self.send((key.to_s + "=").to_s, value)
+        self.send((key.to_s + "="), value)
       end
     }
   end
